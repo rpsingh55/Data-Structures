ee0c5bd7e0cf4af17af99e0fc80e59e893e18796
rpsingh55 <ram.singh_cs19@gla.ac.in>
2021-11-28 22:24:22 +0530

    DOne

--- 60_EditDistance_string.cpp
+++ 60_EditDistance_string.cpp
@@ -3,9 +3,10 @@
 #include<map>
 #include<string>
 #include<set>
+#define pb push_back
 using namespace std;
 
-    int editDistance(string s, string t) {
+   int editDistance(string s, string t) {
         int m = s.length();
         int n = t.length();
         int arr[m+1][n+1];
@@ -28,10 +29,20 @@ using namespace std;
             }
         }
         return arr[m][n];
-    }     	
+    }
 
+void displayvector(vector<int> v){
+for(auto i:v){
+    cout<<i<<" ";
+}
+cout<<"\n";
+}
+void displayarray(int a[],int n){
+for(int i = 0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+cout<<"\n";
+}
 int main(){
-
 cout<<editDistance("geek","gesek");
-
 }
