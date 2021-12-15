e57dd3863852ca7c501ccc6092f715a6d8ade0ba
rpsingh55 <ram.singh_cs19@gla.ac.in>
2021-12-15 23:00:46 +0530

    Done

--- 82_AllAnagrams_string.cpp
+++ 82_AllAnagrams_string.cpp
@@ -2,39 +2,20 @@
 #define pb push_back
 using namespace std;
 
-      vector<vector<string> > Anagrams(vector<string>& v) {
-        map<string,int> m;
-        vector<string> temp;
-        int n = v.size();
-        for(auto i : v){
-            string s = i;
-             sort(s.begin(),s.end());
-            temp.push_back(s);
+ string removeConsecutiveCharacter(string s)
+    {
+        int n = s.length();
+        for(int i = 0;i<n-1;i++){
+            if(s[i] == s[i+1]){
+                s.erase(s.begin()+i);
+                n--;
+                i--;
+            }
         }
-            vector<vector<string>> jawab;
-            for(int i = 0;i<n;i++){
-                if(m[temp[i]] == 0){
-                    m[temp[i]] = 1;
-                    vector<string> temp2;
-                    for(int j = i;j<n;j++){
-                        if(temp[i] == temp[j]){
-                            temp2.push_back(v[j]);
-                        }
-                    }
-                    jawab.push_back(temp2);
-                }
-        }
-        return jawab;
+        return s;
     }
 
-
 int main(){
-    vector<string> v;
-    v.pb("act");
-    v.pb("dog");
-    v.pb("odg");
-    v.pb("tac");
-    v.pb("cat");
-    Anagrams(v);
+   cout<<removeConsecutiveCharacter("uuttaar");
 }
  
