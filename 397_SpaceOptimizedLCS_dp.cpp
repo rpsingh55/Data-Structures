b32e3b1a153af9458a853b0cb9fafbb23ffdee9b
rpsingh55 <ram.singh_cs19@gla.ac.in>
2022-01-25 22:54:09 +0530

    Done

---
+++ 397_SpaceOptimizedLCS_dp.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<map>
+#include<string>
+#include<set>
+#define pb push_back
+using namespace std;
+
+ int lcs(int x, int y, string s1, string s2)
+    {
+        int dp[x+1][y+1];
+        for(int i = 0; i <= x;i++){
+            for(int j = 0;j <= y;j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else{
+                    if(s1[i-1] == s2[j-1]){
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[x][y];
+    }
+
+    
+
+int main(){
+ 
+    cout <<lcs(5,6,"fjsgs","sdeafg");
+}
