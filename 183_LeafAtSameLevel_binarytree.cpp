72eda6e93564f69b22f0ad8db7742033ca53a816
rpsingh55 <ram.singh_cs19@gla.ac.in>
2022-03-29 21:07:46 +0530

    Done

---
+++ 183_LeafAtSameLevel_binarytree.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+
+        string currVal = ip[i];
+        if(currVal != "N") {
+            currNode->left = newNode(stoi(currVal));
+            queue.push(currNode->left);
+        }
+            
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+        if(currVal != "N") {
+            currNode->right = newNode(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+
+class Solution{
+  public:
+  
+    int rh = -1;
+  
+    bool prkriya(Node *root, int h){
+        if(root == NULL){
+            return true;
+        }
+
+       if(root->left == NULL && root->right == NULL)
+           if(rh == -1)
+               rh = h;
+           else if(rh != h)
+               return false;
+       
+       bool left = prkriya(root->left, h+1);
+       if(left == false)
+           return false;
+       bool right = prkriya(root->right, h+1);
+      
+       return right;
+        
+    }
+  
+    bool check(Node *root)
+    {
+        return prkriya(root, 0);
+    }
+};
+
+int main()
+{
+    int t;
+    cin>> t;
+    getchar();
+    while (t--)
+    {
+        string s;
+        getline( cin, s );
+        Node* head = buildTree( s );
+        Solution obj;
+        cout << obj.check(head) << endl;
+    }
+    return 0;
+}
