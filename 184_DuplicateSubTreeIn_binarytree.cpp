26ab58e8242410377c4852ac8b3263bf57e953ce
rpsingh55 <ram.singh_cs19@gla.ac.in>
2022-03-30 23:58:50 +0530

    Done

---
+++ 184_DuplicateSubTreeIn_binarytree.cpp
@@ -0,0 +1,130 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(char x) {
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node *root = new Node(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current Node
+			currNode->left = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+
+
+unordered_map<string,int>m;
+
+
+string solve(Node* root)
+{
+   if(root == NULL)
+   {
+       return "$";
+   }
+   
+   string s =" ";
+   
+   if(!root->right and !root->left)
+   {
+        s = to_string(root->data);
+        
+        return s;
+   }
+   
+   s= s+to_string(root->data);
+   s = s+solve(root->left);
+   s = s+solve(root->right);
+   
+   m[s]++;
+   return s;
+}
+         
+  
+    int dupSub(Node *root) {
+         solve(root);
+   
+   for(auto x : m)
+   {
+          if(x.second >=2 )
+          {
+              
+              return 1;
+          }
+   }
+   
+   return 0;
+    }
+};
