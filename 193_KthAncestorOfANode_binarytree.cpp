aff966a39b8c3b7d51c549f57a23067868eea11a
rpsingh55 <ram.singh_cs19@gla.ac.in>
2022-04-12 23:46:41 +0530

    Done

--- 193_KthAncestorOfANode_binarytree.cpp
+++ 193_KthAncestorOfANode_binarytree.cpp
@@ -46,6 +46,7 @@ int main()
 	root->right = newNode(3);
 	root->left->left = newNode(4);
 	root->left->right = newNode(5);
+	root->left->right->left->right = newNode(55555);
 
 	int k = 2;
 	int node = 5;
