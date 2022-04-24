2d4f1a93b58a0b5d7007db2fd2a1a3b698386721
rpsingh55 <ram.singh_cs19@gla.ac.in>
2022-04-24 21:50:37 +0530

    Done

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,146 +1,119 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+struct Node {
     int data;
-    struct Node *left;
-    struct Node *right;
-    struct Node *next;
+    Node *left;
+    Node *right;
 
-    Node(int x)
-    {
-        data = x;
-        left = NULL;
-        right = NULL;
-        next = NULL;
+    Node(int val) {
+        data = val;
+        left = right = NULL;
     }
 };
+Node* buildTree(string str)
+{   
 
-void printInorder(Node *node)
-{
-    if (node == NULL)
-    {
-        return;
-    }
-    printInorder(node->left);
-    cout << node->data << " ";
-    printInorder(node->right);
-}
-Node *buildTree(string str)
-{
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
 
     vector<string> ip;
-
+    
     istringstream iss(str);
-    for (string str; iss >> str;)
+    for(string str; iss >> str; )
         ip.push_back(str);
-
-    Node *root = new Node(stoi(ip[0]));
-
-    queue<Node *> queue;
+        
+    
+    Node* root = new Node(stoi(ip[0]));
+   
+    queue<Node*> queue;
     queue.push(root);
-
+        
+    
     int i = 1;
-    while (!queue.empty() && i < ip.size())
-    {
-
-
-        Node *currNode = queue.front();
+    while(!queue.empty() && i < ip.size()) {
+            
+        
+        Node* currNode = queue.front();
         queue.pop();
-
+       
         string currVal = ip[i];
+ 
+        if(currVal != "N") {
+                
 
-        if (currVal != "N")
-        {
-
-            
             currNode->left = new Node(stoi(currVal));
-
-            
+                
+           
             queue.push(currNode->left);
         }
-
-      
+            
         i++;
-        if (i >= ip.size())
+        if(i >= ip.size())
             break;
         currVal = ip[i];
-
-
-        if (currVal != "N")
-        {
-
+            
+        if(currVal != "N") {
+                
             currNode->right = new Node(stoi(currVal));
-
+                
             queue.push(currNode->right);
         }
         i++;
     }
-
+    
     return root;
 }
 
-Node *Inorder(Node *root)
-{
-    if (root->left == NULL)
-        return root;
-    Inorder(root->left);
-}
-
-
-
-class Solution
-{
-public:
-    //Node* prev=NULL;
-    vector<Node*> v;
-    
-    void inorder(Node *root){
-        if(!root) return;
-        inorder(root->left);
-        v.push_back(root);
-        inorder(root->right);
-    }
-    
-    void populateNext(Node *root)
-    {
-        inorder(root);
-        for(int i = 0; i < v.size()-1; i++){
-            v[i]->next = v[i+1];
-        }
-       /*if(!root)
-       return ;
-       populateNext(root->left);
-       if(prev)
-       prev->next=root;
-       prev=root;
-       populateNext(root->right);*/
-    }
-};
-
+Node* LCA(Node * root , int l , int h);
 
 int main()
 {
+
     int t;
-    scanf("%d", &t);
-    cin.ignore();
-    while (t--)
+	scanf("%d ",&t);
+    while(t--)
     {
-        string treeString;
-        getline(cin, treeString);
-        Node *root = buildTree(treeString);
-        Solution obj;
-        obj.populateNext(root);
-        Node *ptr = Inorder(root);
-        while (ptr)
-        {
-            printf("%d->%d ", ptr->data, ptr->next ? ptr->next->data : -1);
-            ptr = ptr->next;
-        }
-        cout << endl;
+        string s;
+        int l , h;
+		getline(cin,s);
+		scanf("%d ",&l);
+		scanf("%d ",&h);
+        Node* root = buildTree(s);
+        cout<<LCA(root ,  l ,  h)->data<<endl;
     }
-    return 0;
-}  
+    return 1;
+}
+
+Node* LCA(Node *root, int n1, int n2)
+{
+    if(root == NULL) return NULL;
+    if(root->data == n1 || root->data == n2){
+        return root;
+    }
+    Node *l = LCA(root->left, n1, n2);
+    Node *r = LCA(root->right, n1, n2);
+    
+    if(l && r){
+        return root;
+    }
+    else if(l){
+        return l;
+    }
+    else{
+        return r;
+    }
+    /*  if (node == null) {
+           return null;
+       }
+       if(d1<node.data && d2<node.data){
+           return LCA(node.left,d1,d2);
+       }else if(d1>node.data && d2 > node.data){
+           return LCA(node.right,d1,d2);
+       }else{
+           return node;
+       }*/
+}
+
+
