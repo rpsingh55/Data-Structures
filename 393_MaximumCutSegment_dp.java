8183fd5992691629bde343154fc885482a23b95e
rpsingh55 <ram.singh_cs19@gla.ac.in>
2022-06-17 19:44:13 +0530

    Done

--- 393_MaximumCutSegment_dp.java
+++ 393_MaximumCutSegment_dp.java
@@ -22,7 +22,11 @@ class DSA{
        int[] dp = new int[n+1];
        Arrays.fill(dp,-1);
        int jawab =  prkriya(n, x, y, z, dp);
-       return jawab < 0 ? 0 : jawab;
+       //return jawab < 0 ? 0 : jawab;
+       if(jawab < 0){
+           return 0;
+       }
+       return jawab;
     }
     public static void main(String[] args) {
         System.out.println(maximizeCuts(50,2,10,5));
